0beadb315b53496eeb5113b333737236c1169487
Thanhtu1824 <thanhtua20s@gmail.com>
2026-05-26 09:43:44 +0700

    Add User module

--- drf_project/api/user/views.py
+++ drf_project/api/user/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.views import APIView, Http404
 from rest_framework.response import Response
 
-from api.models import User
+from api.user.models import User
 from .serializers import UserSerializers
 
 
@@ -39,8 +39,3 @@ class UserDetail(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        user=self.get_object(pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
